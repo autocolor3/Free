71c5303b87028ea9bfd662b61e280b01dba0a374
auto <hitbydev@proton.me>
2025-05-28 19:03:07 +0100

    Create Bloxburg.lua

---
+++ Bloxburg.lua
@@ -0,0 +1,7 @@
+--// Guide:
+-- 1) Fully load into bloxburg, select a plot and let it load.
+-- 2) Select 'Delivery Person' job, let game load.
+-- 3) Execute script, go afk.
+-- 4) Come back and enjoy the profit!
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/942f2a297ff3a965d4e785881d31ac93.lua"))() -- Made by _color3
