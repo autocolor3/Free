2e47261aad3ee22367647ed3ed4b6d34af7c8465
auto <hitbydev@proton.me>
2025-06-11 20:29:33 +0100

    Update and rename UniversalSilent.lua to UniversalCombat.lua

---
+++ UniversalCombat.lua
@@ -0,0 +1,3 @@
+--[ Universal combat script, supports most games, has esp, silent aim and some other features
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/cdc8872a3c6cfe6654884758e47f6029.lua"))() -- made by _color3
--- UniversalSilent.lua
+++
@@ -1,5 +0,0 @@
---// Universal silent aim, works on most pvp games
---// Created by myself, _color3
-
--- No UI for now, just execute and have fun.
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/eb0e1483484db29dbe110fcb4f92a553.lua"))()
